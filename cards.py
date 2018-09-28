43a678ce572e04a1bd140d27fa3f59412ec8b849
Matt Marum <mmarum1@advancelocal.net>
2018-09-28 12:43:32 -0400

    initial

---
+++ cards.py
@@ -0,0 +1,21 @@
+# From:
+# Ramalho, Luciano. Fluent Python: Clear, Concise, and Effective Programming (p. 5). O'Reilly Media. Kindle Edition. 
+
+import collections
+
+Card = collections.namedtuple('Card', ['rank', 'suit'])
+
+class FrenchDeck:
+    ranks = [str(n) for n in range(2, 11)] + list('JQKA')
+
+    suits = 'spades diamonds clubs hearts'. split()
+
+    def __init__( self):
+        self._cards = [Card(rank, suit) for suit in self.suits for rank in self.ranks]
+
+    def __len__(self):
+        return len(self._cards)
+
+    def __getitem__(self, position):
+        return self._cards[position]
+
